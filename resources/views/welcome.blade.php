426eb3662d5cb04ad78bcbbe0800eeee72bba925
zincri <zincri_mlz@hotmail.com>
2018-08-15 13:06:58 -0500

    Zincri
    Zincri

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Zincri</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laracasts.com">Zincri</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
