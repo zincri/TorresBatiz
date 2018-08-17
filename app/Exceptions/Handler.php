1224c1451dccdd8ddcf857b4ef29762b01d8e806
JesuSelvaS <jassselvas@gmail.com>
2018-08-17 12:40:27 -0500

    Yisus
    Error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,6 +44,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception->getStatusCode() == 404) {
+            return response()->view('errors.error404', [], 404);
+        }
         return parent::render($request, $exception);
     }
 
---
+++ resources/views/errors/error404.blade.php
@@ -0,0 +1,40 @@
+@extends ('layouts.master')
+
+@section ('content')
+
+<div id="inner_banner" class="section inner_banner_section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="full">
+                        <div class="title-holder">
+                            <div class="title-holder-cell text-left">
+                                <h1 class="page-title">404 Error</h1>
+                                <ol class="breadcrumb">
+                                    <li><a href="index.html">Inicio</a></li>
+                                    <li class="active">404 Error</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end inner page banner -->
+    <!-- section -->
+    <div class="section padding_layout_1">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="center margin_bottom_30_all"> <img class="margin_bottom_30_all" style="width: 400px; height: 400px" src="images/error404.svg" alt="#"> </div>
+                    <div class="heading text_align_center">
+                        <h2>OOOPS, NO PUDIMOS ENCONTRAR ESTA PAGINA!</h2>
+                    </div>
+                    <div class="center" style="margin-bottom: 2%"> <a class="btn sqaure_bt light_theme_bt" href="home1.html">Regresar a inicio</a> </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
