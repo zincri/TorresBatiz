1451f2312da32df2e292795235ec7eb2a46a40aa
JuanraCamacho <juanch00@live.com.mx>
2018-08-17 12:12:51 -0500

    Juanra
    Algo

--- resources/views/navbar/productos.blade.php
+++ resources/views/navbar/productos.blade.php
@@ -42,7 +42,7 @@
                         <img class="img-responsive" src="images/layout_img/ser1.jpg" alt="#">
                      </div>
                      <div class="service_head">
-                        <h5><a href="blog.html">Todos los productos</a></h5>
+                        <h5><a href="/productostodos">Todos los productos</a></h5>
                      </div>
                   </div>
                </div>
@@ -52,7 +52,7 @@
                         <img class="img-responsive" src="images/layout_img/ser2.jpg" alt="#">
                      </div>
                      <div class="service_head">
-                        <h5><a href="blog.html">Almacenaje <br>(locker,gabinetes y estantería)</a></h5>
+                        <h5><a href="/productostodos">Almacenaje <br>(locker,gabinetes y estantería)</a></h5>
                      </div>
                   </div>
                </div>
@@ -62,7 +62,7 @@
                         <img class="img-responsive" src="images/layout_img/ser3.jpg" alt="#">
                      </div>
                      <div class="service_head">
-                        <h5><a href="blog.html">Confortables</a></h5>
+                        <h5><a href="/productostodos">Confortables</a></h5>
                      </div>
                   </div>
                </div>
@@ -72,7 +72,7 @@
                         <img class="img-responsive" src="images/layout_img/ser4.jpg" alt="#">
                      </div>
                      <div class="service_head">
-                        <h5><a href="blog.html">Control de asistencias (Biométricos y reloj checador)</a></h5>
+                        <h5><a href="/productostodos">Control de asistencias (Biométricos y reloj checador)</a></h5>
                      </div>
                   </div>
                </div>
@@ -82,7 +82,7 @@
                         <img class="img-responsive" src="images/layout_img/ser6.jpg" alt="#">
                      </div>
                      <div class="service_head">
-                        <h5><a href="blog.html">Cajas fuertes</a></h5>
+                        <h5><a href="/productostodos">Cajas fuertes</a></h5>
                      </div>
                   </div>
                </div>
@@ -92,7 +92,7 @@
                         <img class="img-responsive" src="images/layout_img/ser4.jpg" alt="#">
                      </div>
                      <div class="service_head">
-                        <h5><a href="blog.html">Equipos Multimedia y soluciones educativas</a></h5>
+                        <h5><a href="/productostodos">Equipos Multimedia y soluciones educativas</a></h5>
                      </div>
                   </div>
                </div>
@@ -103,7 +103,7 @@
                         <img class="img-responsive" src="images/layout_img/ser5.jpg" alt="#">
                      </div>
                      <div class="service_head">
-                        <h5><a href="blog.html">Mobiliario de madera</a></h5>
+                        <h5><a href="/productostodos">Mobiliario de madera</a></h5>
                      </div>
                   </div>
                </div>
@@ -113,7 +113,7 @@
                         <img class="img-responsive" src="images/layout_img/ser6.jpg" alt="#">
                      </div>
                      <div class="service_head">
-                        <h5><a href="blog.html">Mobiliario metálico</a></h5>
+                        <h5><a href="/productostodos">Mobiliario metálico</a></h5>
                      </div>
                   </div>
                </div>
@@ -123,7 +123,7 @@
                         <img class="img-responsive" src="images/layout_img/ser1.jpg" alt="#">
                      </div>
                      <div class="service_head">
-                        <h5><a href="blog.html">Mobiliario escolar</a></h5>
+                        <h5><a href="/productostodos">Mobiliario escolar</a></h5>
                      </div>
                   </div>
                </div>
@@ -133,7 +133,7 @@
                         <img class="img-responsive" src="images/layout_img/ser2.jpg" alt="#">
                      </div>
                      <div class="service_head">
-                        <h5><a href="blog.html">Mesas de juntas</a></h5>
+                        <h5><a href="/productostodos">Mesas de juntas</a></h5>
                      </div>
                   </div>
                </div>
@@ -143,7 +143,7 @@
                         <img class="img-responsive" src="images/layout_img/ser3.jpg" alt="#">
                      </div>
                      <div class="service_head">
-                        <h5><a href="blog.html">Multifuncionales y solución documental</a></h5>
+                        <h5><a href="/productostodos">Multifuncionales y solución documental</a></h5>
                      </div>
                   </div>
                </div>
@@ -154,7 +154,7 @@
                         <img class="img-responsive" src="images/layout_img/ser5.jpg" alt="#">
                      </div>
                      <div class="service_head">
-                        <h5><a href="blog.html">Recepciones</a></h5>
+                        <h5><a href="/productostodos">Recepciones</a></h5>
                      </div>
                   </div>
                </div>
@@ -164,7 +164,7 @@
                         <img class="img-responsive" src="images/layout_img/ser4.jpg" alt="#">
                      </div>
                      <div class="service_head" style="margin-bottom: 3%">
-                        <h5><a href="blog.html">Sillería</a></h5>
+                        <h5><a href="/productostodos">Sillería</a></h5>
                      </div>
                   </div>
                </div>
